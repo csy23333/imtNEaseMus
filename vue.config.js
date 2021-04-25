d8d6924d27e1370911880f96bc524a6b28d3e13b
Carr <917244295@qq.com>
2021-04-25 13:48:19 +0800

    delete publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
-  publicPath: "./",
+  // publicPath: "./",
   configureWebpack: {
-    //以下三行代码用来解决生成 dist 文件夹后获取不到图片资源问题
     resolve: {
       // extensions:[]
       alias: {
